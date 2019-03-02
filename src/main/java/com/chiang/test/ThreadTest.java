b8af303929ec0e66e0b29adcfbcdb4c957f8e8e5
jyp <jyp@192.168.0.17>
2019-03-03 06:56:06 +0800

    Thread Init

---
+++ src/main/java/com/chiang/test/ThreadTest.java
@@ -0,0 +1,15 @@
+package com.chiang.test;
+
+public class ThreadTest implements Runnable{
+
+	@Override
+	public void run() {
+		System.out.println("test");
+	}
+	public static void main(String[]args)
+	{
+		Thread test = new Thread(new ThreadTest());
+		test.start();
+	}
+
+}
